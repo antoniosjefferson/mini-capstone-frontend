aaa01cc339df3ded838554946d1acfc0430cd374
antoniosjefferson <itsmehtone@gmail.com>
2022-08-04 12:12:05 -0700

    adds front end validations and http cat

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProductsView from "../views/ProductsView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/all_products",
+    name: "all_products",
+    component: ProductsView,
   },
   {
     path: "/about",
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    axios.get("/all_products.json").then((response) => {
+      this.products = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <h1>THIS SHOULD WORK</h1>
+  <div class="all_products">
+    <div v-for="product in products" v-bind:key="product.name">
+      <h1>{{ product.name }}</h1>
+    </div>
+  </div>
+</template>
+
+<style></style>
